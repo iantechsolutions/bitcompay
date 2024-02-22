4f0aca9c7cd313deb63d95732e929b0445b0d9f4
Tomás Cichero <martoxdlol@gmail.com>
2024-02-22 19:18:25 -0300

    organización

---
+++ diseño.md
@@ -0,0 +1,25 @@
+#
+
+## Flujo de operación
+
+### Cliente de Bitcompay (empleado de una empresa random)
+
+1. Sube un excel con transacciones a cobrar por Bitcompay
+2. Listo nada, se queda ahí tomando mate tranca. (puede hacer otras cosas pero no tiene ganas)
+
+### Empleado normal de Bitcompay
+
+1. Genera un archivo de entrada por cada canal en base a las transacciones cargadas en la DB desde los excel subidos por los clientes.
+2. Lo sube al banco.
+3. Recibe un archivo de salida, se sube al sistema de Bitcompay.
+
+
+## Organizar secciones
+
+### Empleado normal de Bitcompay: "Gestión de documentos"
+
+- Transacciones (ver todas y detalles) -> SUPER VISTA CON MUCHAS COSAS
+- Documentos (ver todos y detalles)
+- Entrada (generar archivo de entrada por canal)
+- Archivos de proceso (archivos de entrada generados)
+- Cargar respuesta
--- src/components/app-sidenav.tsx
+++ src/components/app-sidenav.tsx
@@ -1,21 +1,27 @@
 import Sidenav, { SidenavItem, SidenavSeparator } from "./sidenav"
-import { ActivityIcon, ActivitySquareIcon, BanknoteIcon, DollarSignIcon, FileUpIcon, FingerprintIcon, LayoutDashboardIcon, MessageCircleQuestionIcon, MessageSquareReplyIcon, Settings2Icon, UsersIcon } from 'lucide-react';
+import { ActivityIcon, ActivitySquareIcon, ArrowRightLeftIcon, BadgeDollarSignIcon, BanknoteIcon, Building2Icon, DollarSignIcon, FileLineChart, FileTextIcon, FileUpIcon, FingerprintIcon, LayoutDashboardIcon, MessageCircleQuestionIcon, MessageSquareReplyIcon, Settings2Icon, ShoppingBagIcon, TextIcon, UsersIcon } from 'lucide-react';
 
 export default function AppSidenav() {
     return <Sidenav>
         <SidenavSeparator>Global</SidenavSeparator>
         <SidenavItem icon={<LayoutDashboardIcon />} href="/dashboard">Dashboard</SidenavItem>
         <SidenavSeparator>Administración</SidenavSeparator>
-        <SidenavItem icon={<Settings2Icon />} href="/dashboard/admin/global">Global</SidenavItem>
+        <SidenavItem icon={<Building2Icon />} href="/dashboard/admin/global">Empresas</SidenavItem>
+        <SidenavItem icon={<FileLineChart />} href="/dashboard/admin/global">Canales</SidenavItem>
+        <SidenavItem icon={<ShoppingBagIcon />} href="/dashboard/admin/products">Productos</SidenavItem>
         <SidenavItem icon={<UsersIcon />} href="/dashboard/admin/users">Usuarios</SidenavItem>
         <SidenavItem icon={<FingerprintIcon />} href="/dashboard/admin/roles">Roles</SidenavItem>
         <SidenavItem icon={<DollarSignIcon />} href="/dashboard/admin/costs">Costos</SidenavItem>
         <SidenavItem icon={<BanknoteIcon />} href="/dashboard/admin/cc">Cuenta corriente</SidenavItem>
         <SidenavItem icon={<ActivityIcon />} href="/dashboard/admin/monitoring">Monitoreo dinámico</SidenavItem>
+        <SidenavSeparator>Gestión Bitcompay</SidenavSeparator>
+        <SidenavItem icon={<BadgeDollarSignIcon />} href="/dashboard/management/transactions">Transacciones</SidenavItem>
+        <SidenavItem icon={<FileTextIcon />} href="/dashboard/management/documents">Documentos subidos</SidenavItem>
+        <SidenavItem icon={<ArrowRightLeftIcon />} href="/dashboard/management/generate">Archivos de entrada</SidenavItem>
+        <SidenavItem icon={<MessageSquareReplyIcon />} href="/dashboard/management/response">Respuesta</SidenavItem>
         <SidenavSeparator>Gestión documental</SidenavSeparator>
-        <SidenavItem icon={<FileUpIcon />} href="/dashboard/documents/uploads">Subida</SidenavItem>
-        <SidenavItem icon={<MessageSquareReplyIcon />} href="/dashboard/documents/response">Respuesta</SidenavItem>
-        <SidenavItem icon={<ActivitySquareIcon />} href="/dashboard/documents/monitoring">Monitoreo</SidenavItem>
-        <SidenavItem icon={<MessageCircleQuestionIcon />} href="/dashboard/documents/support">Soporte</SidenavItem>
+        <SidenavItem icon={<FileUpIcon />} href="/dashboard/company/uploads">Subida</SidenavItem>
+        <SidenavItem icon={<ActivitySquareIcon />} href="/dashboard/company/monitoring">Monitoreo</SidenavItem>
+        <SidenavItem icon={<MessageCircleQuestionIcon />} href="/dashboard/company/support">Soporte</SidenavItem>
     </Sidenav>
 }
