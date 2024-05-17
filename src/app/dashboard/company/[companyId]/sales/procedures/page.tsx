687db40300ed0f1c4608fcd0183864e06851dcf4
cayllahuapedro <cayllahuapedro@gmail.com>
2024-05-17 14:26:52 -0300

    refactor: Update procedure list query to use correct API endpoint

--- src/app/dashboard/company/[companyId]/sales/procedures/page.tsx
+++ src/app/dashboard/company/[companyId]/sales/procedures/page.tsx
@@ -5,7 +5,7 @@ import { api } from "~/trpc/server";
 import AddProcedure from "./add-procedure";
 export default async function Page(props: { params: { companyId: string } }) {
   // cambiar luego por tramite router
-  const procedures = await api.bussinessUnits.list.query();
+  const procedures = await api.procedure.list.query();
   return (
     <LayoutContainer>
       <section className="space-y-2">
@@ -18,7 +18,7 @@ export default async function Page(props: { params: { companyId: string } }) {
             <ListTile
               key={procedure.id}
               href={`/dashboard/company/${props.params.companyId}/administration/bussiness_units/${procedure.id}`}
-              title={procedure.description}
+              title={procedure.id}
             />
           ))}
         </List>
