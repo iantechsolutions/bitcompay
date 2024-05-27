c1bd7e0b8af069c1b3e5dbde28c492418705dfd2
Maximo <MaximoCorsani@gmail.com>
2024-05-27 18:32:11 -0300

    .

--- src/app/dashboard/[companyId]/maintenance/tables/page.tsx
+++ src/app/dashboard/[companyId]/maintenance/tables/page.tsx
@@ -1,25 +1,55 @@
-import { api } from "~/trpc/server";
 import { Title } from "~/components/title";
 import { List, ListTile } from "~/components/list";
 import LayoutContainer from "~/components/layout-container";
-import { CircleUserRound } from "lucide-react";
+import { api } from "~/trpc/server";
 
-export default async function Page(props: { params: { companyId: string } }) {
+
+export default async function Maintenance(props: { params: { companyId: string } }) {
+    const items: string[] = [
+        "uploaded_documents",
+        "genders",
+        "civil_status",
+        "parental",
+        "modos",
+        "health_insurances",
+        "market_levels",
+        "financial_entities",
+        "status",
+        "receipt",
+        "taxes",
+        "items",
+        "concepts",
+        "bussiness_unit",
+        "categories",
+        "Nomenclator",
+        "state",
+        "countries",
+        "zone",
+        "afip_status",
+        "iva",
+        "ingresos_brutos",
+        "postal_codes"
+    ];
   return (
     <LayoutContainer>
       <section className="space-y-2">
         <div className="flex justify-between">
-          <Title>Tablas</Title>
-          <List>
-            <ListTile
-              key={1}
-              leading={"Modos"}
-              href={`/dashboard/faIKDivwt7Z8Gp-B5yFrv/maintenance/tables/modos`}
-              title={"Modos"}
-            />
-          </List>
+          <Title>Mantenimiento</Title>
         </div>
+        <List>
+          {items.map((items) => {
+            return (
+              <ListTile
+                key={items}
+                href={`/dashboard/faIKDivwt7Z8Gp-B5yFrv/maintenance/tables/${items}`}
+
+                title={items}
+              />
+            );
+          })}
+        </List>
       </section>
     </LayoutContainer>
   );
 }
+
