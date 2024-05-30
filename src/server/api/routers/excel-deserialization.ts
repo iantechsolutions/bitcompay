c3449d33f513ff46a83c2f2303e41b25af02cfba
cayllahuapedro <cayllahuapedro@gmail.com>
2024-05-30 16:03:09 -0300

    added empty excel check

--- src/server/api/routers/excel-deserialization.ts
+++ src/server/api/routers/excel-deserialization.ts
@@ -258,7 +258,12 @@ async function readExcelFile(db: DBTX, id: string, type: string | undefined) {
   const trimmedRows = rows.map(trimObject);
   const { finishedArray: transformedRows, errors: errorsTransform } =
     recRowsTransformer(trimmedRows);
-
+  if (transformedRows.length === 0) {
+    throw new TRPCError({
+      code: "BAD_REQUEST",
+      message: "No se encontraron datos en el archivo",
+    });
+  }
   const errors: string[] = [];
   errorsTransform.forEach((error) => {
     errors.push(
