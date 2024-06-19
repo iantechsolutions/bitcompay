41e76e47fd27788da546e1e7e3c2860db2920e40
Maximo <MaximoCorsani@gmail.com>
2024-06-19 15:58:24 -0300

    Fixed table affiliate

--- src/app/dashboard/[companyId]/management/client/affiliates/page.tsx
+++ src/app/dashboard/[companyId]/management/client/affiliates/page.tsx
@@ -20,34 +20,36 @@ export default function Home() {
 <Table>
   <TableCaption>Tabla de grupos familiares</TableCaption>
   <TableHeader>
-    <TableRow className="flex">
-      <TableHead className="flex-1 text-left">Grupo familiar</TableHead>
-      <TableHead className="flex-1 text-left">Integrantes</TableHead>
-      <TableHead className="flex-1 text-left">Estado</TableHead>
-      <TableHead className="flex-1 text-left"></TableHead>
+    <TableRow>
+      <TableHead>Grupo familiar</TableHead>
+      <TableHead>Integrantes</TableHead>
+      <TableHead>Estado</TableHead>
+      <TableHead className="text-right"></TableHead>
     </TableRow>
   </TableHeader>
   <TableBody>
     {grupo ? (
       grupo.map((grupo) => (
-        <TableRow key={grupo.id} className="flex">
-          <TableCell className="flex-1 font-medium text-left">{grupo.numericalId}</TableCell>
-          <TableCell className="flex-1  font-medium text-center">{grupo.integrants.length}</TableCell>
-          <TableCell className="flex-1 text-center">{grupo.state}</TableCell>
-          <td className="flex-1">
+        <TableRow key={grupo.id}>
+          <TableCell>{grupo.numericalId}</TableCell>
+          <TableCell className="text-justify">{grupo.integrants.length}</TableCell>
+          <TableCell className="text-justify">{grupo.state}</TableCell>
+          <TableCell className="text-center"></TableCell>
+          <TableCell className="text-right">
             <AffiliatesInfo grupoId={grupo.id} />
-          </td>
+          </TableCell>
         </TableRow>
       ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={4} className="text-center">
-                  No hay afiliados disponibles.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+    ) : (
+      <TableRow>
+        <TableCell colSpan={4} className="text-center">
+          No hay afiliados disponibles.
+        </TableCell>
+      </TableRow>
+    )}
+  </TableBody>
+</Table>
+
 
        
       </section>
