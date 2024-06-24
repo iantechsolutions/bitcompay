9bd164c57b60b6fc6717736a6c83ebc867c568c7
juli2kapo <sestojulian@gmail.com>
2024-06-24 14:58:49 -0300

    added maxDuration to excel read page

--- src/app/dashboard/[companyId]/management/documents/massive-upload/[uploadId]/page.tsx
+++ src/app/dashboard/[companyId]/management/documents/massive-upload/[uploadId]/page.tsx
@@ -2,6 +2,8 @@ import { Title } from "~/components/title";
 import { api } from "~/trpc/server";
 import UnconfirmedPage from "./unconfirmed-page";
 
+export const maxDuration = 300;
+
 export default async function page(props: {
   params: { uploadId: string; companyId: string };
 }) {
