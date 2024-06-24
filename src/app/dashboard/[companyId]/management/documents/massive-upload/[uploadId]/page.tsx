e06575ce4beca3699a3c4fcd1a9e2f5ffba82aa6
juli2kapo <sestojulian@gmail.com>
2024-06-24 15:18:00 -0300

    fixed vercel.json

--- src/app/dashboard/[companyId]/management/documents/massive-upload/[uploadId]/page.tsx
+++ src/app/dashboard/[companyId]/management/documents/massive-upload/[uploadId]/page.tsx
@@ -2,7 +2,9 @@ import { Title } from "~/components/title";
 import { api } from "~/trpc/server";
 import UnconfirmedPage from "./unconfirmed-page";
 
-export const maxDuration = 60;
+export const config = {
+  maxDuration: 60,
+};
 
 export default async function page(props: {
   params: { uploadId: string; companyId: string };
