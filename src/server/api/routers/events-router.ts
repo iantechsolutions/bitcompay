604c04a66e89df8070e3404bd936043d58fdb8c1
juli2kapo <sestojulian@gmail.com>
2024-07-04 17:21:34 -0300

    added method to events router and fixed LastEvent in upload router

--- src/server/api/routers/events-router.ts
+++ src/server/api/routers/events-router.ts
@@ -28,4 +28,68 @@ export const eventsRouter = createTRPCRouter({
 
       return event;
     }),
+  createByType: protectedProcedure
+    .input(
+      z.object({
+        ccId: z.string().max(255),
+        type: z.string().max(255),
+        amount: z.number().max(255),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const lastEvent = await db.query.events.findFirst({
+        orderBy: [desc(schema.events.createdAt)],
+        where: eq(schema.events.currentAccount_id, input.ccId),
+      });
+      if (input.type == "REC") {
+        const new_event = await db
+          .insert(schema.events)
+          .values({
+            description: "Recaudacion",
+            currentAccount_id: input.ccId,
+            type: input.type,
+            event_amount: input.amount,
+            current_amount: (lastEvent?.current_amount ?? 0) + input.amount,
+          })
+          .returning();
+        return new_event;
+      }
+      if (input.type == "FC") {
+        const new_event = await db
+          .insert(schema.events)
+          .values({
+            description: "Factura Creada",
+            currentAccount_id: input.ccId,
+            type: input.type,
+            event_amount: input.amount * -1,
+            current_amount: (lastEvent?.current_amount ?? 0) - input.amount,
+          })
+          .returning();
+        return new_event;
+      }
+      if (input.type == "NC") {
+        const new_event = await db
+          .insert(schema.events)
+          .values({
+            description: "Nota de credito",
+            currentAccount_id: input.ccId,
+            type: input.type,
+            event_amount: input.amount,
+            current_amount: (lastEvent?.current_amount ?? 0) + input.amount,
+          })
+          .returning();
+        return new_event;
+      }
+
+      // const new_plan = await db
+      //   .insert(schema.plans)
+      //   .values({
+      //     user: user?.id ?? "",
+      //     plan_code: input.plan_code,
+      //     description: input.description,
+      //     brand_id: input.brand_id,
+      //   })
+      //   .returning();
+      // return new_plan;
+    }),
 });
