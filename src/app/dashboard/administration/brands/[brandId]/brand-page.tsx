61f4cab5aaad41be52b218e0a1bb663804fdaa5d
Maximo <MaximoCorsani@gmail.com>
2024-07-17 16:13:43 -0300

    .

--- src/app/dashboard/administration/brands/[brandId]/brand-page.tsx
+++ src/app/dashboard/administration/brands/[brandId]/brand-page.tsx
@@ -29,13 +29,6 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "~/components/ui/alert-dialog";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "~/components/ui/select";
 import { Button } from "~/components/ui/button";
 import { Card } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
