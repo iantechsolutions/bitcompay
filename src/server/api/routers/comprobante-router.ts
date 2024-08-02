ebf6d3fb69efa98c655315b0987591a78b8a89a5
Anselmo <anseldalbene@gmail.com>
2024-08-02 12:30:40 -0300

    test

--- src/server/api/routers/comprobante-router.ts
+++ src/server/api/routers/comprobante-router.ts
@@ -160,7 +160,7 @@ async function approbatecomprobante(liquidationId: string) {
       .update(schema.liquidations)
       .set({ estado: "aprobada", userApproved: user?.id })
       .where(eq(schema.liquidations.id, liquidationId));
-    const afip = await ingresarAfip();
+    // const afip = await ingresarAfip();
     let last_voucher;
     // const browser = await chromium.puppeteer.launch({
     //   args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
@@ -176,10 +176,11 @@ async function approbatecomprobante(liquidationId: string) {
       const comprobanteCod =
         comprobanteDictionary[comprobante.tipoComprobante ?? ""];
       try {
-        last_voucher = await afip.ElectronicBilling.getLastVoucher(
-          comprobante?.ptoVenta,
-          comprobanteCod
-        );
+        // last_voucher = await afip.ElectronicBilling.getLastVoucher(
+        //   comprobante?.ptoVenta,
+        //   comprobanteCod
+        // );
+        last_voucher = 0;
       } catch {
         last_voucher = 0;
       }
