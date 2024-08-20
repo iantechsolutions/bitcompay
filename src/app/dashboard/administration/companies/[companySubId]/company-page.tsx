d49ad6a87e6b15aff0e994a8126e2c25df8535d6
juli2kapo <sestojulian@gmail.com>
2024-08-20 14:27:10 -0300

    changed zones

--- src/app/dashboard/administration/companies/[companySubId]/company-page.tsx
+++ src/app/dashboard/administration/companies/[companySubId]/company-page.tsx
@@ -114,7 +114,7 @@ export default function CompanyPage({
 
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
-            <AccordionTrigger>
+            <AccordionTrigger className="border-b">
               <h2 className="text-md">Productos habilitados</h2>
             </AccordionTrigger>
             <AccordionContent>
