e3e9bb709d6e12a7ed5d86dd259774cf735a3465
juli2kapo <sestojulian@gmail.com>
2024-08-23 09:46:32 -0300

    fixed sidenav spacing for smaller screens

--- src/components/applayout.tsx
+++ src/components/applayout.tsx
@@ -44,7 +44,7 @@ export default async function AppLayout(props: AppLayoutProps) {
 
       <aside
         // biome-ignore lint/nursery/useSortedClasses: <explanation>
-        className={`fixed bottom-0 left-0 z-20 hidden max-h-full w-[25%]  bg-white overflow-y-hidden md:block ${props.sidenavClass}`}
+        className={`fixed bottom-0 left-0 z-20 hidden max-h-full md:w-[25%]  bg-white overflow-y-hidden md:block ${props.sidenavClass}`}
       >
         {props.sidenav}
       </aside>
