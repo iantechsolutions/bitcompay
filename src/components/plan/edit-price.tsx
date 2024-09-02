660f10e0e931980f07d0f9f96269a4846fbeb87f
cayllahuapedro <cayllahuapedro@gmail.com>
2024-09-02 19:05:36 -0300

    little design corrections

--- src/components/plan/edit-price.tsx
+++ src/components/plan/edit-price.tsx
@@ -35,10 +35,9 @@ export default function EditPrice({
   const [percent, setPercent] = useState("");
   const [open, setOpen] = useState(false);
   const { mutateAsync: createPricePerAge, isLoading } =
-  api.pricePerCondition.create.useMutation();
-  const router=useRouter();
+    api.pricePerCondition.create.useMutation();
+  const router = useRouter();
   async function handleUpdatePrice() {
-    
     if (plan?.pricesPerCondition) {
       if (
         plan?.pricesPerCondition.filter(
@@ -97,179 +96,198 @@ export default function EditPrice({
         </DropdownMenuContent>
       </DropdownMenu>
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className="sm:max-w-[600px] overflow-y-visible">
-          <DialogHeader>
+        <DialogContent className="sm:max-w-[600px] overflow-y-visible m-3 rounded-2xl">
+          <DialogHeader className="p-2 ml-2">
             <DialogTitle>Actualizar porcentualmente precio de plan</DialogTitle>
           </DialogHeader>
-          <Label htmlFor="validy_date">Mes de vigencia</Label>
-          <Select
-            onValueChange={(e) => setMes(Number(e))}
-            defaultValue={mes.toString()}
-          >
-            <SelectTrigger>
-              <SelectValue placeholder="Seleccione un mes" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem
-                value="0"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 0, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Enero
-              </SelectItem>
-              <SelectItem
-                value="1"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 1, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Febrero
-              </SelectItem>
-              <SelectItem
-                value="2"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 2, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Marzo
-              </SelectItem>
-              <SelectItem
-                value="3"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 3, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Abril
-              </SelectItem>
-              <SelectItem
-                value="4"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 4, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Mayo
-              </SelectItem>
-              <SelectItem
-                value="5"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 5, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Junio
-              </SelectItem>
-              <SelectItem
-                value="6"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 6, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Julio
-              </SelectItem>
-              <SelectItem
-                value="7"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 7, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Agosto
-              </SelectItem>
-              <SelectItem
-                value="8"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 8, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Septiembre
-              </SelectItem>
-              <SelectItem
-                value="9"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() === new Date(anio, 9, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Octubre
-              </SelectItem>
-              <SelectItem
-                value="10"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() ===
-                      new Date(anio, 10, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Noviembre
-              </SelectItem>
-              <SelectItem
-                value="11"
-                disabled={
-                  plan?.pricesPerCondition?.filter(
-                    (x) =>
-                      x.validy_date.getTime() ===
-                      new Date(anio, 11, 1).getTime()
-                  ).length !== 0
-                }
-              >
-                Diciembre
-              </SelectItem>
-            </SelectContent>
-          </Select>
-          <div>
+          <div className="w-1/4 text-gray-500 mb-2 ml-3">
+            <Label htmlFor="validy_date">Mes de vigencia</Label>
+            <Select
+              onValueChange={(e) => setMes(Number(e))}
+              defaultValue={mes.toString()}
+            >
+              <SelectTrigger className="w-full border-[#BEF0BB] border-0 border-b text-[#3E3E3E] bg-background rounded-none focus-visible:ring-green-400">
+                <SelectValue placeholder="Seleccione un mes" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem
+                  value="0"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 0, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Enero
+                </SelectItem>
+                <SelectItem
+                  value="1"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 1, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Febrero
+                </SelectItem>
+                <SelectItem
+                  value="2"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 2, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Marzo
+                </SelectItem>
+                <SelectItem
+                  value="3"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 3, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Abril
+                </SelectItem>
+                <SelectItem
+                  value="4"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 4, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Mayo
+                </SelectItem>
+                <SelectItem
+                  value="5"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 5, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Junio
+                </SelectItem>
+                <SelectItem
+                  value="6"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 6, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Julio
+                </SelectItem>
+                <SelectItem
+                  value="7"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 7, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Agosto
+                </SelectItem>
+                <SelectItem
+                  value="8"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 8, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Septiembre
+                </SelectItem>
+                <SelectItem
+                  value="9"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 9, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Octubre
+                </SelectItem>
+                <SelectItem
+                  value="10"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 10, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Noviembre
+                </SelectItem>
+                <SelectItem
+                  value="11"
+                  disabled={
+                    plan?.pricesPerCondition?.filter(
+                      (x) =>
+                        x.validy_date.getTime() ===
+                        new Date(anio, 11, 1).getTime()
+                    ).length !== 0
+                  }
+                >
+                  Diciembre
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="w-1/4 text-gray-500 mb-2 ml-3">
             <Label>Año de Vigencia</Label>
             <Input
-              className="border-green-300 focus-visible:ring-green-400 w-[100px]"
+              className="w-full border-[#BEF0BB] border-0 border-b text-[#3E3E3E] bg-background rounded-none "
               type="number"
+              min={new Date().getFullYear()}
               value={anio}
               onChange={(e) => setAnio(Number(e.target.value))}
             />
           </div>
 
-          <div>
+          <div className="w-1/4 text-gray-500 mb-2 ml-3">
             <Label htmlFor="number">Porcentaje de aumento</Label>
             <Input
               id="number"
+              className="w-full border-[#BEF0BB] border-0 border-b text-[#3E3E3E] bg-background rounded-none"
               placeholder="Ej: 30%"
               value={percent}
               onChange={(e) => setPercent(e.target.value)}
             />
           </div>
           <div>
-            <Button disabled={isLoading} onClick={handleUpdatePrice}>
+            <Button
+              disabled={isLoading}
+              onClick={handleUpdatePrice}
+              className="bg-[#BEF0BB] hover:bg-[#BEF0BB] ml-3 rounded-full mr-4 px-6 text-black font-normal hover:text-[#3E3E3E]"
+            >
               {isLoading && (
                 <Loader2Icon className="mr-2 animate-spin" size={20} />
               )}
+              <CreditCardPosIcon className="mr-2 h-5 font-medium-medium"/>
               Actualizar precio
             </Button>
           </div>
