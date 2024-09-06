0257478fbde459df6d674e92c4178e01823900f9
marcfbob <msbobzin@gmail.com>
2024-09-06 15:26:40 -0300

    fixed sidenav highliting and added relationships in accordion in clients afiliates

--- src/components/company-sidenav.tsx
+++ src/components/company-sidenav.tsx
@@ -212,7 +212,7 @@ export default function CompanySidenav() {
       </div>
       <Sidenav className="w-[20vw] bg-white px-[1vw] pl-[2vw]">
         <SidenavItem
-          className="w-[11vw]  pl-[1vw] text-sideNav"
+          className="w-[11vw]  bg-white pl-[1vw] text-sideNav"
           href={`/dashboard/home`}
           icon={
             <img
