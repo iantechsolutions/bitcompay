a40034b2e68d950bedeb3a8cad586349fa669d81
juli2kapo <info@iantech.com.ar>
2024-10-01 08:17:35 -0300

    removed maxduration due to build error

--- src/app/billing/liquidation/[liquidationId]/page.tsx
+++ src/app/billing/liquidation/[liquidationId]/page.tsx
@@ -37,7 +37,6 @@ import RejectLiquidationDialog from "./reject-liquidation-dialog";
 import { ChevronLeft, CircleX } from "lucide-react";
 import { GoBackButton } from "~/components/goback-button";
 
-export const maxDuration = 300;
 export default async function Home(props: {
   params: { liquidationId: string };
 }) {
