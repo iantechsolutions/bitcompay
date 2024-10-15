264b0ef5ccc648ab0e7af7b326e50051d5226e17
marcfbob <msbobzin@gmail.com>
2024-10-15 19:27:49 -0300

    smaller sidebar

--- src/components/company-sidenav.tsx
+++ src/components/company-sidenav.tsx
@@ -205,14 +205,14 @@ export default function CompanySidenav() {
     <ScrollArea className="h-[99vh] w-full relative overflow-x-auto">
       <div className="sticky top-0 bg-white w-full h-[11vh] px-[1vw] items-center justify-center content-center text-center z-20">
         <img
-          className="max-w-52 w-[20vw] px-auto py-auto"
+          className="max-w-52 w-[18vw] px-auto py-auto"
           src="/public/bitcom-03.png"
           alt="logo"
         />
       </div>
-      <Sidenav className="w-[20vw] bg-white px-[1vw] pl-[2vw]">
+      <Sidenav className="w-[18vw] bg-white pr-2 pl-4">
         <SidenavItem
-          className="w-[11vw] bg-white pl-[1vw] text-sideNav"
+          className="w-[11vw] bg-white text-sideNav"
           href={`/`}
           icon={
             <img
