17080ca822a18473cf282ef8e6f09b858e6a9762
G - D <tomas9842@gmail.com>
2024-10-15 11:08:21 -0300

    reload después de manual issuance

--- src/app/billing/manual_issuance/page.tsx
+++ src/app/billing/manual_issuance/page.tsx
@@ -76,6 +76,7 @@ export default function Page() {
   const [selectedComprobante, setSelectedComprobante] = useState<any>(null);
   const [afip, setAfip] = useState<any>(null);
   const router = useRouter();
+
   useEffect(() => {
     async function loginAfip() {
       const afip = await ingresarAfip();
@@ -133,28 +134,21 @@ export default function Page() {
     }
 
     if (
-      tipoComprobante != "0" &&
-      (
-      !form.getValues().puntoVenta ||
-      !form.getValues().dateEmision ||
-      !tipoComprobante ||
-      !concepto ||
-      !iva ||
-      !form.getValues().dateVencimiento ||
-      !subTotal ||
-      !tributos
-    )
-
-    ||
-
-    tipoComprobante == "0" &&
-    (
-      !form.getValues().dateEmision ||
-      !tipoComprobante ||
-      !form.getValues().dateVencimiento ||
-      !subTotal ||
-      !tributos
-    )
+      (tipoComprobante != "0" &&
+        (!form.getValues().puntoVenta ||
+          !form.getValues().dateEmision ||
+          !tipoComprobante ||
+          !concepto ||
+          !iva ||
+          !form.getValues().dateVencimiento ||
+          !subTotal ||
+          !tributos)) ||
+      (tipoComprobante == "0" &&
+        (!form.getValues().dateEmision ||
+          !tipoComprobante ||
+          !form.getValues().dateVencimiento ||
+          !subTotal ||
+          !tributos))
     ) {
       console.log(!form.getValues().puntoVenta);
       console.log(!form.getValues().dateEmision);
@@ -513,6 +507,7 @@ export default function Page() {
       facturasEmitidas: 0,
     },
   });
+
   const conceptsForm = useForm<ConceptsForm>({
     defaultValues: {
       concepts: [{ concepto: "", importe: 0, iva: 0, total: 0 }],
@@ -526,6 +521,7 @@ export default function Page() {
       ],
     },
   });
+
   console.log("comprobante tipo", valueToNameComprobanteMap[tipoComprobante]);
   const asociatedFCForm = useForm<AsociatedFCForm>({
     defaultValues: {
@@ -546,12 +542,13 @@ export default function Page() {
     defaultValues: { otherConcepts: [{ description: "", importe: 0 }] },
   });
 
+  const [brandId, setBrandId] = useState("");
   const [page, setPage] = useState<"formPage" | "confirmationPage">("formPage");
+
   function handlePageChange(page: "formPage" | "confirmationPage") {
     setPage(page);
   }
 
-  const [brandId, setBrandId] = useState("");
   function handleGrupoFamilarChange(value: string) {
     setGrupoFamiliarId(value);
     setObraSocialId("");
@@ -582,15 +579,50 @@ export default function Page() {
     setIvaCondition(obra?.afip_status ?? "");
     setSellCondition("No Aplica");
   }
+
   function handleComprobanteChange(value: string) {
     setFCSelec(value);
     setSelectedComprobante(comprobantes?.find((x) => x.id == value));
   }
+
   let selectedBrand;
 
   const handleBrandChange = (value: string) => {
     selectedBrand = marcas?.find((marca: { id: string }) => marca.id === value);
     setBrandId(value);
+  };
+
+  const reloadPage = () => {
+    setSubTotal(0);
+    setIvaTotal(0);
+    setOtherAttributes(0);
+    setLogo("");
+    setFCSelec("");
+    setFcSeleccionada([]);
+    setComprobantes(undefined);
+    setSelectedComprobante(null);
+    setTipoComprobante("");
+    setCreatedComprobante(undefined);
+    setTipoDocumento("");
+    setIvaCondition("");
+    setSellCondition("");
+    setNroDocumento("");
+    setNroDocumentoDNI("");
+    setNombre("");
+    setTributos("0");
+    setservicioprod("Servicio");
+    setObraSocialId("");
+    setError(null);
+    // setLoading(true);
+    setGrupoFamiliarId("");
+    form.reset();
+    conceptsForm.reset();
+    otherTributesForm.reset();
+    asociatedFCForm.reset();
+    otherConceptsForm.reset();
+    setPage("formPage");
+    setBrandId("");
+    router.refresh();
   };
 
   if (!grupoFamiliarId && !obraSocialId) {
@@ -790,6 +822,7 @@ export default function Page() {
         {page === "confirmationPage" && createdComprobante && (
           <ConfirmationPage
             form={form}
+            reloadPage={reloadPage}
             fcSeleccionada={fcSeleccionada}
             setFcSeleccionada={setFcSeleccionada}
             conceptsForm={conceptsForm}
