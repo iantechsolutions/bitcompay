fa68efa59de3c88a1ff2ed23cc7dc52d239dd484
juli2kapo <sesto@elykia.com.ar>
2024-10-16 17:52:22 -0300

    moved buttons to the left

--- src/components/manual_issuance/confirmation-page.tsx
+++ src/components/manual_issuance/confirmation-page.tsx
@@ -532,7 +532,7 @@ const confirmationPage = ({
 
 
 {finishedAFIP && 
-      <div className="self-end flex gap-1">
+      <div className=" self-start flex gap-1">
           <Button
           className="h-7 bg-[#BEF0BB] hover:bg-[#BEF0BB] text-[#3e3e3e] font-medium-medium text-sm rounded-2xl py-4 px-4 shadow-none"
           onClick={() => {
